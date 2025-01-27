06c9754bac439e4dfabcc3fcaa6607fce7ac6321
Aarij khan <aarij0100@gmail.com>
2025-01-27 07:32:52 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# list = [1,2,3,4,5,6,7,8,9,10]
+# list.append(11)
+# list.remove(5)
+# print(list)
+
+# fruits = ['apple', 'banana', 'cherry']
+# fruits.insert(0, "orange")
+# print(fruits)
+
+# fruits = ['apple', 'banana', 'cherry']
+# list = [1,2,3,4,5,6,7,8,9,10]
+# fruits.extend(list)
+# print('result: ', fruits)
+
+# list = [1,2,3,4,5,6,7,8,9,10]
+# x = list.pop()
+# print('x: ', x)
+# print('x: ', list)
+# names = ['John', 'Jane', 'Doe', 'Alice']
+# x = names.index('Doe')
+# print('x: ', x)
+
+# nums = [2, 3, 5, 7, 11, 13]
+# for num in nums[:]:
+#     print(num)
+#     # if num > 10:
+#     #     nums.remove(num)  
+
+# # print(nums)
