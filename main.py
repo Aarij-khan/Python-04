6d16227562fd2efea692fb8a320af1dbc598a6ec
Aarij khan <aarij0100@gmail.com>
2025-01-27 07:48:50 +0500

    done

--- main.py
+++ main.py
@@ -27,3 +27,32 @@
 #     #     nums.remove(num)  
 
 # # print(nums)
+
+# numbers = [5, 3, 8, 1, 2]
+# numbers.sort()
+# print(numbers)
+
+# colors = ['red', 'blue', 'green']
+# colors.reverse()
+# print(colors)
+
+# words = ['apple', 'apple', 'banana', 'cherry']
+# x = words.count("apple")
+# print('x: ', x)
+
+# original = [1, 2, 3]
+# local = original.copy()
+# print('local: ', local)
+
+# tasks = ['task1', 'task2', 'task3']
+# tasks.clear()
+# print('x: ', tasks)
+
+# list1 = []
+# list = [1,2,3,4,5,6,7,8,9,10]
+# for i in list:
+#     list1.append(i * i)
+# print(list1)    
+    
+    
+
